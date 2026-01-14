c8747d9c0bd3ff3ca01f9af4760f548fcd061f3e
azerothxx <friskyxygel@gmail.com>
2026-01-14 13:30:41 +0800

    Dashboard working

---
+++ src/app/components/dashboard.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { Circle } from "lucide-react";
+import Link from "next/link";
+import { useState } from "react";
+
+export default function Dashboard() {
+    const [isHovered, setIsHovered] = useState(false);
+    const items = [
+        { text: "Home", link: "/" },
+        { text: "Projects", link: "/projects" },
+        { text: "Pricing", link: "/pricing" },
+        { text: "TOS", link: "/tos" },
+    ];
+
+    return (
+        <div
+            id="dashboard"
+            className="w-1/12 h-screen bg-background dark:bg-black"
+        >
+            <div className="flex flex-row mt-5 h-1/4">
+                <div
+                    id="dashboard-circle"
+                    className="flex flex-col w-1/3 items-end"
+                >
+                    <Circle size="2.5rem" className="py-3 mr-1" />
+                </div>
+                <div id="dashboard-buttons" className="flex flex-col">
+                    {items.map((item) => (
+                        <Link className="" href={item.link} key={item.text}>
+                            {item.text}
+                        </Link>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+}
